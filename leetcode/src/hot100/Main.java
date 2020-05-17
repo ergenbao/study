f38e5ba8d5405db72b3c154404676b8e623a984e
jianghao <haojiang05@yangqianguan.com>
2020-05-17 11:35:25 +0800

    删除了三个

--- leetcode/src/hot100/Main.java
+++ leetcode/src/hot100/Main.java
@@ -192,8 +192,5 @@ public class Main {
         map.put(647,"CountSubStrings");
         //739、每日温度
         map.put(739,"DailyTemperatures");
-        map.put(999,"test2");
-        map.put(000,"test3");
-        map.put(111,"test4");
     }
 }
